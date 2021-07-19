c4bb415f927871c51aeeece0bde01cf5b0f5b2ed
reachsaxena <80594627+reachsaxena@users.noreply.github.com>
2021-07-19 00:33:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+$("#change-font").on("click",
+ function() {
+    $("#change-font").html("font-family: cursive;");
+ });
+
+$("#change-background-color").on("click",
+ function() {
+    $("#change-background-color").html("background <br> :gray;");
+ });
+
+ $("#change-the-font-size").on("click",
+ function() {
+    $("#change-the-font-size").html("font-size:x-large;");
+ });
+
+ $("#change-the-text-color").on("click",
+ function() {
+    $("#change-the-text-color").html("color:red;");
+ });
+
+ $("#change-the-font-weight").on("click",
+ function() {
+    $("#change-the-font-weight").html("font-weight: <br> lighter;");
+ });
+
+ $("#change-the-font-style").on("click",
+ function() {
+    $("#change-the-font-style").html("font-style: italic;");
+ });
+
+ $("#add-the-line-from-text").on("click",
+ function() {
+    $("#add-the-line-from-text").html("text-decoration: underline;");
+ });
+
+ $("#make-a-linear-gradient").on("click",
+ function() {
+    $("#make-a-linear-gradient").html("background: linear-gradient(45 <br>  deg, red, blue);");
+ });
+
+ $("#change-x-index").on("click",
+ function() {
+    $("#change-x-index").html("z-index: 3;");
+ });
+
+ $("#add-a-border").on("click",
+ function() {
+    $("#add-a-border").html("border: 3px solid red;");
+ });
+
+
+ //add-a-border
+
+ 
